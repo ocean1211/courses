5b6165f06908323cc582e39fff13c0f776b26d9e
Vuong Hoang <vuonghv.cs@gmail.com>
2017-04-26 16:37:09 +0700

    Implement lab 1

--- cs144/lab12/ctcp.c
+++ cs144/lab12/ctcp.c
@@ -16,6 +16,8 @@
 #include "ctcp_sys.h"
 #include "ctcp_utils.h"
 
+#define CTCP_HDR_SIZE sizeof(ctcp_segment_t)
+
 /**
  * Connection state.
  *
@@ -39,6 +41,9 @@ struct ctcp_state {
                                this if this is the case for you */
 
   /* FIXME: Add other needed fields. */
+  uint32_t seqno;
+  uint32_t ackno;
+  int data_len;
 };
 
 /**
@@ -49,6 +54,27 @@ static ctcp_state_t *state_list;
 
 /* FIXME: Feel free to add as many helper functions as needed. Don't repeat
           code! Helper functions make the code clearer and cleaner. */
+
+/*
+ * returns: The number of bytes actually sent, 0 if nothing was sent, or -1 if
+ *          there was an error.
+ */
+int send_ctcp(ctcp_state_t *state, uint32_t flags, char *data, int len) {
+    int segment_len = CTCP_HDR_SIZE + len;
+    ctcp_segment_t *segment = malloc(segment_len);
+    segment->seqno = htonl(state->seqno);
+    segment->ackno = htonl(state->ackno);
+    segment->len = htons(segment_len);
+    segment->flags = htonl(flags);
+    segment->window = MAX_SEG_DATA_SIZE;
+    segment->cksum = 0;
+    if (len > 0) {
+        memcpy(segment->data, data, len);
+    }
+    segment->cksum = cksum(segment, segment_len);
+    state->data_len = len;
+    return conn_send(state->conn, segment, segment_len);
+}
 
 
 ctcp_state_t *ctcp_init(conn_t *conn, ctcp_config_t *cfg) {
@@ -69,6 +95,10 @@ ctcp_state_t *ctcp_init(conn_t *conn, ctcp_config_t *cfg) {
   /* Set fields. */
   state->conn = conn;
   /* FIXME: Do any other initialization here. */
+  state->seqno = 1;
+  state->ackno = 1;
+  state->data_len = 0;
+  free(cfg);
 
   return state;
 }
@@ -89,10 +119,60 @@ void ctcp_destroy(ctcp_state_t *state) {
 
 void ctcp_read(ctcp_state_t *state) {
   /* FIXME */
+    char *data = malloc(MAX_SEG_DATA_SIZE);
+    if (!data) {
+        fprintf(stderr, "Out of memory");
+        exit(EXIT_FAILURE);
+    }
+
+    int data_len = conn_input(state->conn, data, MAX_SEG_DATA_SIZE);
+
+    if (data_len == 0) {
+        free(data);
+        return;
+    } else if (data_len == -1) {    // Error or EOF
+        send_ctcp(state, FIN, NULL, 0);
+        //ctcp_destroy(state);
+        free(data);
+        return;
+    }
+
+    send_ctcp(state, ACK, data, data_len);
+    free(data);
 }
 
 void ctcp_receive(ctcp_state_t *state, ctcp_segment_t *segment, size_t len) {
-  /* FIXME */
+    /* FIXME */
+    // Just an ACK packet
+    size_t data_len = len - CTCP_HDR_SIZE;
+    if ((segment->flags & htonl(ACK)) && (data_len == 0)) {
+         //&& (state->seqno + state->data_len) == ntohl(segment->ackno)) {
+        state->seqno = ntohl(segment->ackno);
+        free(segment);
+        return;
+    }
+
+    // Handle duplicated segments
+    /*
+    if ((ntohl(segment->seqno) + data_len) < state->ackno) {
+        free(segment);
+        return;
+    }
+    */
+
+    state->ackno = ntohl(segment->seqno) + data_len;
+    send_ctcp(state, ACK, NULL, 0);
+    ctcp_output(state);
+
+    if (segment->flags & ntohl(FIN)) {
+        conn_output(state->conn, NULL, 0);
+        ctcp_destroy(state);
+        return;
+    }
+
+    conn_output(state->conn, segment->data, data_len);
+
+    free(segment);
 }
 
 void ctcp_output(ctcp_state_t *state) {
